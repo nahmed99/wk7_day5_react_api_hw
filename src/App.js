4a21d5941414a93fbb03d06d5fc467e898a28459
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-18 17:19:01 +0000

    fetch and a useState coded in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [surahs, setSurahs] = useState([])
+
+  const fetchSurahs = () => {
+    const url = "http://api.alquran.cloud/v1/surah";
+
+    fetch(url)  //return a promise
+      .then((res) => res.json())  // return another promise
+      .then((jsonData) => {
+        setSurahs(jsonData);
+      });
+
+      console.log(surahs);
+  };
+
+
   return (
     <>
       <h1>This is the main page!</h1>
