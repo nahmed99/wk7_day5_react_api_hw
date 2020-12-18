59aac30c01ba2017cbd9fa201a5f07154b542350
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-18 17:37:47 +0000

    Added useEffect and placed the fetch function within this (to stop Chrome issuing dependency warnings)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const [surahs, setSurahs] = useState([])
+  const [surahs, setSurahs] = useState([]);
 
-  const fetchSurahs = () => {
-    const url = "http://api.alquran.cloud/v1/surah";
+  // const fetchSurahs = () => {
+  //   const url = "http://api.alquran.cloud/v1/surah";
 
-    fetch(url)  //return a promise
-      .then((res) => res.json())  // return another promise
-      .then((jsonData) => {
-        setSurahs(jsonData);
-      });
+  //   fetch(url)  //return a promise
+  //     .then((res) => res.json())  // return another promise
+  //     .then((jsonData) => {
+  //       setSurahs(jsonData);
+  //     });
 
-      console.log(surahs);
-  };
+  //     console.log(surahs);
+  // };
+
+
+  // This useEffect() will load the data once, upon initial entry to the app.
+  useEffect(() => {
+
+    const fetchSurahs = () => {
+      const url = "http://api.alquran.cloud/v1/surah";
+  
+      fetch(url)  //return a promise
+        .then((res) => res.json())  // return another promise
+        .then((jsonData) => {
+          setSurahs(jsonData);
+        });
+
+    };
+  
+    fetchSurahs();
+  }, []);  //The [] as the second argument ensures that fetchSurahs is only involed once.
+
+  console.log(surahs);
 
 
   return (
